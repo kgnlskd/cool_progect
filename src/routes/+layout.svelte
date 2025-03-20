18f5ac6ee0a2f6a25e58e2311be6c90d0e873c60
stud <stud@it233-2.almetpt.local>
2025-03-20 18:25:11 +0300

    Поменяла хедер

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import Header from "$Lib/components/Header.svelte";
+    import Header from "$lib/components/Header.svelte";
     import {Container, Styles} from "@sveltestrap/sveltestrap";
 
+    const {children} = $props();
 </script>
 
 <Styles/>
